a0e16ffeb2d2c52739cd8986c571a3879e05d29f
Sean Nguyen <sean.nguyentrong@gmail.com>
2015-05-30 13:32:59 +0800

    Added validation input box

--- client/app/topics/topics.controller.js
+++ client/app/topics/topics.controller.js
@@ -4,7 +4,7 @@ var app = angular.module('ratingApp');
 var STATE = {search: 'search', view: 'view'};
 
 app.controller('TopicsController', function($rootScope, $scope) {
-	$scope.state = STATE.search;
+	$scope.state = STATE.view;
 	$scope.search = {keyword: ''};
 	$scope.showedTopics = $rootScope.dataController.getTopics();
 	$scope.currentTopic = $scope.showedTopics[0];
