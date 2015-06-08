488533b78de5686752ddd81237f87761e12c702d
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-06-08 11:13:23 +0800

    added "api" prefix in front of all apis

--- server/routes.js
+++ server/routes.js
@@ -13,34 +13,34 @@ module.exports = function(app) {
   //app.use('/api/things', require('./api/thing'));
 
   /* User */
-  app.get('/users', controllers.user.retrieveAll);
-  app.get('/users/:id', controllers.auth.requireUser(), controllers.user.retrieve);
-  app.post('/users', controllers.user.create);
-  app.post('/users/login', controllers.user.login);
-  app.post('/users/logout', controllers.auth.requireUser(), controllers.user.logout);
-  app.put('/users/:id', controllers.auth.requireUser(), controllers.user.update);
+  app.get('/api/users', controllers.user.retrieveAll);
+  app.get('/api/users/:id', controllers.auth.requireUser(), controllers.user.retrieve);
+  app.post('/api/users', controllers.user.create);
+  app.post('/api/users/login', controllers.user.login);
+  app.post('/api/users/logout', controllers.auth.requireUser(), controllers.user.logout);
+  app.put('/api/users/:id', controllers.auth.requireUser(), controllers.user.update);
   app.delete('/users/:id', controllers.auth.requireUser(), controllers.user.delete);
   
   /* Article */
-  app.get('/articles', controllers.article.retrieveAll);
-  app.post('/articles', controllers.article.create);
-  app.get('/articles/:id', controllers.article.retrieve);
-  app.put('/articles/:id', controllers.article.update);
-  app.delete('/articles/:id', controllers.article.delete);
+  app.get('/api/articles', controllers.article.retrieveAll);
+  app.post('/api/articles', controllers.article.create);
+  app.get('/api/articles/:id', controllers.article.retrieve);
+  app.put('/api/articles/:id', controllers.article.update);
+  app.delete('/api/articles/:id', controllers.article.delete);
 
   /* Asset */
-  app.get('/assets', controllers.asset.retrieveAll);
-  app.post('/assets', controllers.asset.create);
-  app.get('/assets/:id', controllers.asset.retrieve);
-  app.put('/assets/:id', controllers.asset.update);
-  app.delete('/assets/:id', controllers.asset.delete);
+  app.get('/api/assets', controllers.asset.retrieveAll);
+  app.post('/api/assets', controllers.asset.create);
+  app.get('/api/assets/:id', controllers.asset.retrieve);
+  app.put('/api/assets/:id', controllers.asset.update);
+  app.delete('/api/assets/:id', controllers.asset.delete);
 
   /* Price */
-  app.get('/prices', controllers.price.retrieveAll);
-  app.post('/prices', controllers.price.create);
-  app.get('/prices/:id', controllers.price.retrieve);
-  app.put('/prices/:id', controllers.price.update);
-  app.delete('/prices/:id', controllers.price.delete);
+  app.get('/api/prices', controllers.price.retrieveAll);
+  app.post('/api/prices', controllers.price.create);
+  app.get('/api/prices/:id', controllers.price.retrieve);
+  app.put('/api/prices/:id', controllers.price.update);
+  app.delete('/api/prices/:id', controllers.price.delete);
 
   /* Company */
   app.get('/companies', controllers.company.retrieveAll);
@@ -50,11 +50,11 @@ module.exports = function(app) {
   app.delete('/companies/:id', controllers.company.delete);
 
   /* People */
-  app.get('/people', controllers.person.retrieveAll);
-  app.post('/people', controllers.person.create);
-  app.get('/people/:id', controllers.person.retrieve);
-  app.put('/people/:id', controllers.person.update);
-  app.delete('/people/:id', controllers.person.delete);
+  app.get('/api/people', controllers.person.retrieveAll);
+  app.post('/api/people', controllers.person.create);
+  app.get('/api/people/:id', controllers.person.retrieve);
+  app.put('/api/people/:id', controllers.person.update);
+  app.delete('/api/people/:id', controllers.person.delete);
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
