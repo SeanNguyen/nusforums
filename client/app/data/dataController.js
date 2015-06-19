66aa62f13db2a5763c7fad9392146e659bbb097b
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-06-19 19:47:25 +0800

    adjust predictor and asset view

--- client/app/data/dataController.js
+++ client/app/data/dataController.js
@@ -32,9 +32,8 @@ app.run(function ($http, $rootScope) {
 });
 
 function DataController () {
-	var predictors = [new PredictorModel("1", "Jim Rogers", 'James Beeland "Jim" Rogers, Jr. (born October 19, 1942) is an American businessman, investor and author. He is currently based in Singapore. Rogers is the Chairman of Rogers Holdings and Beeland Interests, Inc. He was the co-founder of the Quantum Fund and creator of the Rogers International Commodities Index (RICI). Rogers does not consider himself a member of any school of economic thought, but has acknowledged that his views best fit the label of Austrian School of economics.'), 
-						new PredictorModel("2", "Mark Mobius", 'Joseph Mark Mobius (born August 17, 1936) is an emerging markets fund manager at Franklin Templeton Investments. Mark Mobius, Ph.D., executive chairman of Templeton Emerging Markets Group, joined Templeton in 1987. Currently, he directs the Templeton research team based in 18 global emerging markets offices and manages emerging markets portfolios.')];
-
+	var predictors = [];
+	
 	var topics = [];
 
 	var assets = [];
