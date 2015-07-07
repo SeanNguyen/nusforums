8e86d3bb32064662389718ce2d9cd8eaeb518d62
Thien Tran <thientran1707@gmail.com>
2015-07-08 02:13:45 +0800

    leaderboard tags

--- client/app/leaderBoard/leaderBoard.controller.js
+++ client/app/leaderBoard/leaderBoard.controller.js
@@ -4,5 +4,18 @@ var app = angular.module('ratingApp');
 app.controller('leaderBoardController', function($rootScope, $scope) {
   $scope.userList = $rootScope.dataController.getUsers();
   $scope.predictorList = $rootScope.dataController.getPredictors();
-  $scope.isHiden = true;
+  
+  // by default, all divs are invisible
+  $scope.userVisible = false;
+  $scope.predictorVisible = false;
+
+  $scope.showUser = function() {
+  	$scope.userVisible = true;
+  	$scope.predictorVisible = false;
+  };
+
+  $scope.showPredictor = function() {
+  	$scope.userVisible = false;
+  	$scope.predictorVisible = true;
+  };
 });
