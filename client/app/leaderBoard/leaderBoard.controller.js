031c1dca94e245b582e7405e9c18732815e22db9
Thien Tran <thientran1707@gmail.com>
2015-07-08 02:23:44 +0800

    refresh leaderboard

--- client/app/leaderBoard/leaderBoard.controller.js
+++ client/app/leaderBoard/leaderBoard.controller.js
@@ -2,19 +2,23 @@
 var app = angular.module('ratingApp');
 
 app.controller('leaderBoardController', function($rootScope, $scope) {
-  $scope.userList = $rootScope.dataController.getUsers();
-  $scope.predictorList = $rootScope.dataController.getPredictors();
+  $scope.userList = [];
+  $scope.predictorList = [];
   
   // by default, all divs are invisible
   $scope.userVisible = false;
   $scope.predictorVisible = false;
 
   $scope.showUser = function() {
+  	// refresh the list when button clicked
+  	$scope.userList = $rootScope.dataController.getUsers();
   	$scope.userVisible = true;
   	$scope.predictorVisible = false;
   };
 
   $scope.showPredictor = function() {
+  	// refresh the list when button clicked
+  	$scope.predictorList = $rootScope.dataController.getPredictors();
   	$scope.userVisible = false;
   	$scope.predictorVisible = true;
   };
