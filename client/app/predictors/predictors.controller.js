d74316f47e919997b4dc1c174ec7d83e7b7f62e9
seannguyen <firestormkjc.1993@gmail.com>
2015-07-27 16:00:11 +0800

    added title filter to the predictors search

--- client/app/predictors/predictors.controller.js
+++ client/app/predictors/predictors.controller.js
@@ -2,16 +2,18 @@
 var app = angular.module('ratingApp');
 
 app.controller('PredictorsController', function($rootScope, $scope) {
-	$scope.search = {keyword: ''};
+	$scope.search = {name: '', title: ''};
 	$scope.showedPredictors = $rootScope.dataController.getPredictors();
 
-	$scope.onSearch = function() {
+	$scope.onSearch = function(name, title) {
 		var predictors = $rootScope.dataController.getPredictors();
+		name = name.toLowerCase();
+		title = title.toLowerCase();
 		$scope.showedPredictors = [];
 		for (var i = predictors.length - 1; i >= 0; i--) {
 			var predictorName = predictors[i].commonName.toLowerCase();
-			var keyword = $scope.search.keyword.toLowerCase();
-			if(predictorName.includes(keyword)) {
+			var predictorTitle = predictors[i].currentTitle.toLowerCase();
+			if(predictorName.includes(name) && predictorTitle.includes(title)) {
 				$scope.showedPredictors.push(predictors[i]);
 			}
 		}
