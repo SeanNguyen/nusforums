fbc27d7f875946976d88835f4443fe8743738850
Thien Tran <thientran1707@gmail.com>
2015-09-14 00:51:34 -0700

    add functions

--- client/app/services/statistic.service.js
+++ client/app/services/statistic.service.js
@@ -2,20 +2,24 @@ var app = angular.module('ratingApp');
 
 app.factory('statistic', ['$q', '$rootScope', '$resource', function ($q, $rootScope, $resource) {
   
+  // price and asset class
+  var Price = $resource('/api/prices')
   function getReturnRate(assetId, startDate, endDate) {
     var startPrice = $resource('prices/', {}, {
-      query: {method: 'GET', params: {id: assetId, date: startDate}, isArray: true}
+      query: {method: 'GET', params: {id: assetId, startDate: startDate, endDate: startDate}, isArray: true}
     });
 
     var endPrice = $resource('prices/', {}, {
-      query: {method: 'GET', params: {id: assetId, date: endDate}, isArray: true}
+      query: {method: 'GET', params: {id: assetId, startDate: endDate, endDate: endDate}, isArray: true}
     });
 
-    return endPrice/startPrice -1;
+    return endPrice.adjClose/startPrice.adjClose -1;
   };
 
   function getReturnRateByAssetAndPredictor(assetId, predictorId, startDate, endDate) {
+    // retrieve assestIds predicted by predictor
 
+    var assetIds = $resource('')
   };
 
   function getReturnRateByPredictor(predictorId, startDate, endDate) {
